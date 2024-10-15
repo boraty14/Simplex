6dae546bdf66b4329ae951dfa9c0f7e188b6ed14
boraty14 <bratygame14@gmail.com>
2024-10-15 21:03:09 +0300

    vcontainer settings

--- Assets/Game/Scripts/Gameplay/GameRunner.cs
+++ Assets/Game/Scripts/Gameplay/GameRunner.cs
@@ -1,30 +1,15 @@
-using Game.Scripts.GameTask;
-using Game.Scripts.Memory;
-using Game.Scripts.Panel;
-using Game.Scripts.Scene;
-using Game.Scripts.Unit;
 using UnityEngine;
+using VContainer;
 
 namespace Game.Scripts.Gameplay
 {
     public class GameRunner : MonoBehaviour
     {
-        [SerializeField] private GameStateData _gameStateData;
+        [Inject] private GameStateData _gameStateData;
         
-        private GameTaskRunner _gameTaskRunner;
-        private SceneLoader _sceneLoader;
-        private UnitRegistry _unitRegistry;
-        private PanelHandler _panelHandler;
-        private DynamicLoader _dynamicLoader;
-
         private void Awake()
         {
-            _gameTaskRunner = new GameTaskRunner();
-            _sceneLoader = new SceneLoader();
-            _unitRegistry = new UnitRegistry();
-            _panelHandler = new PanelHandler();
-            _dynamicLoader = new DynamicLoader();
-            _gameStateData = new GameStateData();
+            Debug.Log(_gameStateData.Id);
         }
     }
 }
--- Assets/Game/Scripts/Scopes/Root/RootScope.cs
+++ Assets/Game/Scripts/Scopes/Root/RootScope.cs
@@ -1,9 +1,24 @@
-using UnityEngine;
+using Game.Scripts.Gameplay;
+using Game.Scripts.GameTask;
+using Game.Scripts.Memory;
+using Game.Scripts.Panel;
+using Game.Scripts.Scene;
+using Game.Scripts.Unit;
+using VContainer;
+using VContainer.Unity;
 
 namespace Game.Scripts.Scopes.Root
 {
-    public class RootScope : MonoBehaviour
+    public class RootScope : LifetimeScope
     {
-        
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<UnitRegistry>(Lifetime.Singleton);
+            builder.Register<GameTaskRunner>(Lifetime.Singleton);
+            builder.Register<SceneLoader>(Lifetime.Singleton);
+            builder.Register<PanelHandler>(Lifetime.Singleton);
+            builder.Register<DynamicLoader>(Lifetime.Singleton);
+            builder.Register<GameStateData>(Lifetime.Singleton);
+        }
     }
 }
