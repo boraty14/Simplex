6f2756cd97004081c2736d0bbe1f5c02804e265e
boraty14 <bratygame14@gmail.com>
2024-10-17 02:05:55 +0300

    added event system to root

--- Assets/Game/Scripts/Scopes/Root/RootScope.cs
+++ Assets/Game/Scripts/Scopes/Root/RootScope.cs
@@ -22,21 +22,20 @@ namespace Game.Scripts.Scopes.Root
             // components
             builder.RegisterComponentInHierarchy<RootUICamera>();
             builder.RegisterComponentInHierarchy<GameCamera>();
-            
+
             // game tasks
             builder.Register<RestartGameTask>(Lifetime.Transient);
-            
+
             // entry points
             builder.RegisterEntryPoint<RootRunner>(Lifetime.Singleton);
-            
+
             // RegisterMessagePipe returns options.
-            var options = builder.RegisterMessagePipe(/* configure option */);
+            var options = builder.RegisterMessagePipe( /* configure option */);
             // Setup GlobalMessagePipe to enable diagnostics window and global function
             builder.RegisterBuildCallback(c => GlobalMessagePipe.SetProvider(c.AsServiceProvider()));
-            
+
             // message pipe events
             //builder.RegisterMessageBroker<DummyEvent>(options);
-            
         }
     }
 }
